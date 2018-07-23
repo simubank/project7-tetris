1ad3b76bc989344f3fb6a875a013b41f9d2d3fd8
Ruobin Li <roubineer@gmail.com>
2018-07-23 19:29:13 -0400

    edit allocation class var

--- myfunds/src/main/java/com/levelup/myfunds/Allocation/allocation.java
+++ myfunds/src/main/java/com/levelup/myfunds/Allocation/allocation.java
@@ -6,20 +6,29 @@ import com.levelup.mutualfunds.rest.BankRestApi;
 
 public class allocation {
 	private int age;
-	private double c;
-	private double w;
+	private double contribution;
+	public double contriEquity;
+	public double contriBonds;
+	private double totalWealth;
 	private int upperAge;
 	private int lowerAge;
 	public String Frankie_ID;
 	public String Frankie_DDA;
 	public String Frankie_SDA;
+	public boolean isRegProfile;
+	public double wealthEquity;
+	public double wealthBonds;
 	
-	public allocation(int age, double c, double w) {
+	public allocation(int age, double sdaBalance) {
 		this.age = age;
-		this.c = c;
-		this.w = w;
+		this.contribution = 0;
+		this.totalWealth = sdaBalance;
+		this.wealthEquity = totalWealth/2.0;
+		this.wealthBonds = totalWealth/2.0;
+		
 		this.upperAge = 65;
 		this.lowerAge = 20;
+		this.isRegProfile= true;
 		this.Frankie_ID = "c9ed522e-13a6-4272-a7f3-2b6dd79b33bc_e8ec148b-e09c-4e79-8c02-b7dfd5aed06d";
 		this.Frankie_DDA = "c9ed522e-13a6-4272-a7f3-2b6dd79b33bc_46d45c9c-18f0-41f4-86eb-491a15e4fac1";
 		this.Frankie_SDA = "c9ed522e-13a6-4272-a7f3-2b6dd79b33bc_edab1b18-7403-4085-ad8d-0d58b43ebeb2";
@@ -33,18 +42,18 @@ public class allocation {
 		return transfer_amt;
 	}
 	
-	public double smartAllocation(boolean isRegProfile) {
+	public double smartAllocation() {
 		double ce;
 		
 		if(isRegProfile) {
 			if(age <= lowerAge) {
-				ce = c;
+				ce = contribution;
 			}
 			else if( age > upperAge) {
 				ce = 0;
 			}
 			else {
-				ce = c - 1;
+				ce = contribution - 1;
 			}
 			
 			return ce;
